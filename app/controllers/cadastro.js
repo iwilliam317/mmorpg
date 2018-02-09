c11cda93de17c3d093d54761df3811fdd45c1409
William <willshinji@gmail.com>
2018-02-09 18:16:26 -0200

    validating form cadastro

--- app/controllers/cadastro.js
+++ app/controllers/cadastro.js
@@ -1,3 +1,21 @@
 module.exports.cadastro = function(application, req, res){
-	res.render('cadastro')
+	res.render('cadastro', {erros:'', dados: ''})
+}
+module.exports.cadastrar = function(application, req, res){
+	
+	let data = req.body;
+	
+	req.assert('nome', 'Nome não pode estar vazio!').notEmpty();
+	req.assert('usuario', 'Usuário não pode estar vazio!').notEmpty();
+	req.assert('senha', 'Senha não pode estar vazia!').notEmpty();
+	req.assert('casa', 'Escolha uma casa!').notEmpty();
+
+	let erros = req.validationErrors();
+
+	if (erros){
+		
+		res.render('cadastro', { erros: erros, dados : data})
+	}
+	
+	res.send('cadastro ok')
 }
