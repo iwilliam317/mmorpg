287e8379fa4e3dfa6f26b284b4ab58a782173796
William <willshinji@gmail.com>
2018-02-27 19:23:25 -0300

    controller for login

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,3 +1,19 @@
 module.exports.carregar_index = function(application, req, res){
-	res.render('index');
+  res.render('index', { erros :'', dados: '' });
+}
+
+module.exports.autenticar = function(application, req, res){
+  let dados = req.body;
+  //res.send(dados)
+
+  req.assert("usuario", "Usuário é obrigatório").notEmpty();
+  req.assert("senha", "Senha é obrigatória").notEmpty();
+
+  let erros = req.validationErrors();
+
+  if (erros){   
+    res.render("index",  {erros: erros, dados: dados}  )
+    //return
+  }
+  res.send("tudo certo")
 }
